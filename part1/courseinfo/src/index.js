ba6a29eb7a05a24c7d81a8fb0fcd608113dfaf81
rasmusjaa <rasmus@lense.fi>
2020-10-03 22:47:50 +0300

    part1 -1.5

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({course}) => {
+	return <h1>{course.name}</h1>
+}
+
+const Part = ({part}) => {
+	return <p>{part.name} {part.exercises}</p>
+}
+
+const Content = ({parts}) => {
+	const [part1, part2, part3] = parts;
+	return (
+		<div>
+			<Part part={part1} />
+			<Part part={part2} />
+			<Part part={part3} />
+		</div>
+	)
+}
+
+const Total = ({parts}) => {
+	const [part1, part2, part3] = parts;
+	return <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+}
+
+const App = () => {
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+			name: 'Fundamentals of React',
+			exercises: 10
+			},
+			{
+			name: 'Using props to pass data',
+			exercises: 7
+			},
+			{
+			name: 'State of a component',
+			exercises: 14
+			}
+		]
+	}
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
