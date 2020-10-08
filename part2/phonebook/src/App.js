4b800f2cf84b1fcff94245507f3179c65721af1a
rasmusjaa <rasmus@lense.fi>
2020-10-08 18:18:26 +0300

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
 
 const App = () => {
-	const [ persons, setPersons ] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	])
+	const [ persons, setPersons ] = useState([])
 	const [ copyPersons, setCopyPersons ] = useState( JSON.parse(JSON.stringify(persons)) )
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+				setCopyPersons(response.data)
+			})
+	}, [])
 
 	return (
 		<div>
