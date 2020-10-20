58a3c17675bfbfda3dd9c570804cde5f3a7de56e
rasmusjaa <rasmus@lense.fi>
2020-10-20 18:34:57 +0300

    part3 phonebook back end and part 2 front end connected and deployed

---
+++ part3/phonebook/README.md
@@ -0,0 +1,5 @@
+## Phonebook application
+
+Uses API to get numbers for phonebook
+
+Online version for API: https://rj-phonebook.herokuapp.com/api/persons
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(express.static('build'))
+
+app.use(cors())
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -80,8 +89,8 @@ app.post('/api/persons', (req, res) => {
 	res.json(person)
 })
 
+app.use(unknownEndpoint)
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
